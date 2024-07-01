2a3ee48fba4b125f6ff65f2e82f1fc72f71afae5
gaborbozo-laptop <bg.bozogabor@gmail.com>
2024-07-02 00:54:12 +0200

    basic spring project

---
+++ libra-view/src/test/java/hu/bozgab/libra_view/LibraViewApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bozgab.libra_view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
