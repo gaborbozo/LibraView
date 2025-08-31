80fd4d7433cb0a7eaf793af3518624f90ddc1c53
gaborbozo-laptop <bg.bozogabor@gmail.com>
2025-08-31 12:52:14 +0200

    Request logging, banner, IDEA technical

---
+++ libra-view-backend/libra-view-master/src/main/java/hu/bozgab/libraview/master/configuration/filter/RequestLoggingFilter.java
@@ -0,0 +1,31 @@
+package hu.bozgab.libraview.master.configuration.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+
+@Slf4j
+@Configuration
+public class RequestLoggingFilter implements WebFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        var request = exchange.getRequest();
+        log.info("""
+                        
+                        --- %s --->>>
+                        %s?%s
+                        """.formatted(
+                        request.getMethod(),
+                        request.getPath().value(),
+                        request.getQueryParams().toString()
+                )
+        );
+        return chain.filter(exchange);
+    }
+
+}
